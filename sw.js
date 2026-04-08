b3bedf61fe42a9aea27f477e5fb07756e94e4811
TheRhyno <theo38das38@gmail.com>
2026-04-08 23:39:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const ASSETS = [
   'manifest.json'
 ];
 
+
 // Installation du service worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
@@ -15,5 +16,28 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SCHEDULE_NOTIF') {
+
+    setTimeout(() => {
+      self.registration.showNotification("Brainrot TCG", {
+        body: "Ça fait 20 secondes ! Reviens tenter ta chance !",
+        icon: "https://i.postimg.cc/fbKwpCBG/LOGO.png", // Ton logo
+        badge: "https://i.postimg.cc/fbKwpCBG/LOGO.png",
+        vibrate: [200, 100, 200]
+      });
+    }, 20000);
+  }
+});
+
+// Action quand on clique sur la notification
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/') // Ouvre ton jeu
   );
 });

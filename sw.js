705fdbed167b3bd2ba66f5e5f133a8c78995a35b
TheRhyno <theo38das38@gmail.com>
2026-04-09 15:24:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'brainrot-v13'; // Change la version ici pour forcer la mise à jour chez les joueurs
+const CACHE_NAME = 'brainrot-v14'; // Change la version ici pour forcer la mise à jour chez les joueurs
 const ASSETS = [
   'index.html',
   'manifest.json'
